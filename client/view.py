00779cbfc2ffb70c0996114a696d9be39e51656d
FirstShanti <steppe.alone@gmail.com>
2020-04-25 16:22:58 +0300

    update API_HOST

--- client/view.py
+++ client/view.py
@@ -1,32 +1,34 @@
 import requests
 from flask import Blueprint, redirect, render_template, flash, abort, jsonify
-from config import DB_HOST
+from config import API_HOST
 
 users = Blueprint('users', __name__, template_folder='templates')
 
 
-@users.route('/users', methods=['GET'])
+@users.route('/users/', methods=['GET'])
 def user_list():
-	res = requests.get(DB_HOST + 'users')
+	res = requests.get(API_HOST + 'users')
 	if res.status_code == 200:
-		user_list = res.json()['data']
+		user_list = res.json()
 		return render_template('index.html', data=user_list)
 	abort(404, description="Invalid url")
 
 
 @users.route('/users/<user_id>', methods=['GET'])
 def get_user(user_id):
-	res = requests.get(DB_HOST + f'users/{user_id}')
+	res = requests.get(API_HOST + f'users/{user_id}')
+	print('\n\nin get\n\n')
 	if res.status_code == 200:
-		user = res.json()['data']
+		user = res.json()
 		return render_template('user_card.html', user=user)
 	flash(f'User {user_id} not found')
 	return redirect('/users')
 
 
-@users.route('/users/delete/<user_id>', methods=['GET'])
+@users.route('/users/delete/<user_id>', methods=['GET', 'DELETE'])
 def delete_user(user_id):
-	res = requests.delete(DB_HOST + f'users/{user_id}')
+	res = requests.delete(API_HOST + f'users/{user_id}')
+	print('in delete')
 	if res.status_code == 200:
 		flash(f"user {res.json()['data']['user']} was deleted")
 		return redirect('/users')
