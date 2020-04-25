59721f2299c4ee076d273d083c9a146204be8bed
FirstShanti <steppe.alone@gmail.com>
2020-04-25 16:28:25 +0300

    add /api to url route, refactoring

--- server/view.py
+++ server/view.py
@@ -8,33 +8,29 @@ collection = mongo.db.get_collection('users')
 users = Blueprint('users', __name__,)
 
 # get list of users
-@users.route('/users/', methods=['GET'])
+@users.route('/api/users/', methods=['GET'])
 def users_list():
 	users = list(collection.find())
 	if users:
-		return jsonify({'message':'ok', 'data':users})
-	else:
-		return jsonify({'message':'error', 'data':'db is empty'}), 404
+		return jsonify(users)
+	return jsonify({'message':'error', 'data':'db is empty'}), 404
 
 # get single user by id
-@users.route('/users/<user_id>', methods=['GET'])
+@users.route('/api/users/<user_id>', methods=['GET'])
 def get_user(user_id):
 	if ObjectId.is_valid(user_id): 
 		user = collection.find_one({'_id':ObjectId(user_id)})
 		if user:
-			return jsonify({'message':'ok', 'data':user})
-		else:
-			return jsonify({'message':'error', 'data':'user not found'}), 404
-	else:
-		return jsonify({'message':'error', 'data':'invalid user id'}), 404
+			return jsonify(user)
+		return jsonify({'message':'error', 'data':'user not found'}), 404
+	return jsonify({'message':'error', 'data':'invalid user id'}), 404
 
 # delete single user by id
-@users.route('/users/<user_id>', methods=['DELETE'])
+@users.route('/api/users/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
 	if ObjectId.is_valid(user_id):
 		if collection.find_one({'_id':ObjectId(user_id)}):
 			collection.delete_one({'_id':ObjectId(user_id)})
 			return jsonify({'message':'ok', 'data':{'status':'deleted', 'user':user_id}})
-		else:
-			return jsonify({'message':'error', 'data':'user not found'}), 404
+		return jsonify({'message':'error', 'data':'user not found'}), 404
 	return jsonify({'message':'error', 'data':'invalid user id'}), 404
