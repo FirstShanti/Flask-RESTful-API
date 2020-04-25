e9f48b7d7afedf9bfa679b33a00a27dd49c87ef3
FirstShanti <steppe.alone@gmail.com>
2020-04-25 16:26:32 +0300

    edit ETL script to fill_db

--- server/main.py
+++ server/main.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 from app import app
 from view import users
-from ETL_users import fetch_data
+from fill_db import fill_db
 
 app.register_blueprint(users)
 
 if __name__=="__main__":
-	fetch_data()
+	fill_db()
 	app.run(host='0.0.0.0', port=5000)
